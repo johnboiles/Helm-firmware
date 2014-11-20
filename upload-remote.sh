a79aa68b9d0e7158cb9ffcf8877ea5e60c4fa009
John Boiles <johnaboiles@gmail.com>
2014-11-19 17:33:52 -0800

    Update upload-remote script to work with just submitted soft reboot code for the teensy_loader_cli

--- upload-remote.sh
+++ upload-remote.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
+# Configure the following two for your system
 HOST=root@boat-pi.local
 LOADER=/root/repos/teensy_loader_cli/teensy_loader_cli
+
+# Shouldn't need to change these
 FIRMWARE_FILE=firmware.hex
-TEMP_DIR=$(ssh root@boat-pi.local 'mktemp -d')
+TEMP_DIR=$(ssh $HOST 'mktemp -d')
 MMCU=mk20dx256
 scp $FIRMWARE_FILE $HOST:$TEMP_DIR
-ssh $HOST "service ser2net stop; $LOADER -mmcu=$MMCU -v -w $TEMP_DIR/$FIRMWARE_FILE; rm -rf $TEMP_DIR; service ser2net start"
+ssh $HOST "service ser2net stop; $LOADER -s -mmcu=$MMCU -v -w $TEMP_DIR/$FIRMWARE_FILE; rm -rf $TEMP_DIR; service ser2net start"

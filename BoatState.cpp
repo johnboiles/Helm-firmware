6238188a95e6c2a6dcaeac290cee380348799417
John Boiles <johnaboiles@gmail.com>
2015-04-11 21:47:27 -0700

    Renaming from BoatBridge to Helm

--- BoatState.cpp
+++ BoatState.cpp
@@ -1,6 +1,6 @@
 //
 //  BoatState.cpp
-//  BoatBridge
+//  Helm
 //
 //  Created by John Boiles on 12/5/14.
 //  Copyright (c) 2014 johnboiles. All rights reserved.
--- BoatState.h
+++ BoatState.h
@@ -1,6 +1,6 @@
 //
 //  BoatState.h
-//  BoatBridge
+//  Helm
 //
 //  Created by John Boiles on 12/5/14.
 //  Copyright (c) 2014 johnboiles. All rights reserved.
